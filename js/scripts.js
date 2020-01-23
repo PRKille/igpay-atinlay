02edb4fd460f309c066adbc239e66cee814ae8eb
katy solovewicz and Patrick Kille <katy@>
2020-01-23 09:57:49 -0800

    add no input error message.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-    
+    $("form").submit(function(event){
+        event.preventDefault();
+        var sentenceInput = $("input#sentence").val();
+        if (!sentenceInput) {
+            alert("Please enter some text.")
+        } else {
+            
+        }
+    });
 });
