426c3c65e8d1de8cbd890da6c273c6bb33e1a0c2
katy solovewicz and Patrick Kille <patrick@>
2020-01-23 11:49:03 -0800

    add consonant, multi-consonant, and qu functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,56 @@
-$(document).ready(function(){
-    $("form").submit(function(event){
-        event.preventDefault();
-        var sentenceInput = $("input#sentence").val();
-        if (!sentenceInput) {
-            alert("Please enter some text.")
+function vowelcheck(word) {
+  var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  var splitWords = word.split("");
+  var ordway;
+  vowels.forEach(function (vowel) {
+    if (splitWords[0].includes(vowel)) {
+      ordway = splitWords.join("") + "way";
+      console.log("function:", ordway);
+    };
+  });
+  return ordway
+};
+
+function consonantCheck(word) {
+  var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "qu", "r", "s", "t", "v", "w", "x", "y", "z", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "QU", "Qu", "qU", "R", "S", "T", "V", "W", "X", "Y", "Z"];
+  var splitWords = word.split("");
+  var ordway;
+  for (index = 0; index < word.length; index++) {
+    consonants.forEach(function (consonant) {
+      if (splitWords[0].includes(consonant)) {
+        if (splitWords[0] === "q" || splitWords[0] === "Q"){
+          var q = splitWords.shift();
+          splitWords.push(q)
+          if (splitWords[0] === "u" || splitWords[0] === "U"){
+            var u = splitWords.shift();
+            splitWords.push(u)
+          }; 
         } else {
-            
-        }
+          var shift = splitWords.shift()
+          splitWords.push(shift);
+        };
+      };
     });
+  };
+  ordway = splitWords.join("") + "ay";
+  console.log("function:", ordway);
+  return ordway;
+};
+
+$(document).ready(function () {
+  $("form").submit(function (event) {
+    event.preventDefault();
+    var sentenceInput = $("input#sentence").val();
+    if (!sentenceInput) {
+      alert("Please enter some text.")
+    } else if (vowelcheck(sentenceInput)) {
+      var esultray = vowelcheck(sentenceInput);
+      console.log(esultray);
+      alert(esultray);
+    } else {
+      var esultray = consonantCheck(sentenceInput);
+      console.log(esultray);
+      alert(esultray);
+    }
+  });
 });
